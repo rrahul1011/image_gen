1c07427e0b8e3618bf3f44ffdd875d0ffcd9d432
rrahul1011 <68853654+rrahul1011@users.noreply.github.com>
2023-08-31 21:36:41 +0530

    updated the code

--- code/image_gen.py
+++ code/image_gen.py
@@ -2,32 +2,31 @@ import streamlit as st
 import requests
 import io
 from PIL import Image
+
 API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
-API_TOKEN = "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"  
+API_TOKEN = "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"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.content
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
 
 def main():
-    st.title("Visualize your imagination")
-    st.markdown("---")
+    st.title("Image Generation from Text Prompt")
 
-    user_input = st.text_input("Enter a your thought !", "")
+    st.write("Enter a text prompt:")
+    user_input = st.text_area("", "")
 
-    if st.button("Generate"):
+    if st.button("Generate Image"):
         if user_input:
-            image_bytes = query({"inputs": user_input,
-                            })
+            payload = {
+                "inputs": user_input,
+            }
+            image_bytes = query(payload)
             image = Image.open(io.BytesIO(image_bytes))
-            st.image(image)
+            st.image(image, caption="Generated Image", use_column_width=True)
         else:
             st.warning("Please enter a text prompt.")
 
-
-
 if __name__ == "__main__":
     main()
-
-
