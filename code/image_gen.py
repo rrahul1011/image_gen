bcbbc2396062492e8c06b4957a96a77b5b07f957
rrahul1011 <68853654+rrahul1011@users.noreply.github.com>
2023-08-31 19:11:21 +0530

    version_1

---
+++ code/image_gen.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+import io
+from PIL import Image
+API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
+API_TOKEN = "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"  
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+
+def main():
+    st.title("Text Prompt and Model Output")
+
+    user_input = st.text_input("Enter a text prompt", "")
+
+    if st.button("Generate"):
+        if user_input:
+            image_bytes = query({"inputs": user_input,
+                            })
+            image = Image.open(io.BytesIO(image_bytes))
+            st.image(image)
+        else:
+            st.warning("Please enter a text prompt.")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
